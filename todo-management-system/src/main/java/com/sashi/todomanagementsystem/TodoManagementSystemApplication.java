c38aa9916d8173735392b616e68e281432ec824e
Sashitharan <rs003@mymail.sim.edu.sg>
2024-06-09 18:10:21 +0800

    init

---
+++ todo-management-system/src/main/java/com/sashi/todomanagementsystem/TodoManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sashi.todomanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoManagementSystemApplication.class, args);
+    }
+
+}
---
+++ todo-management-system/src/test/java/com/sashi/todomanagementsystem/TodoManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sashi.todomanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
